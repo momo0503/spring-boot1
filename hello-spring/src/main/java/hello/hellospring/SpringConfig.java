072374be491ab13f38090dd64b8885a15632fe67
KIMYOUNGMO <yiyi6464@naver.com>
2020-12-17 22:28:20 +0900

    Gradle refresh 하지않아 JDBC 가 작동되지 않았음. Maven에 익숙해져있어서 깜빡했었다. build.gradle에 JPA,H2관련 라이브러리 추가 (application.preoerties) 스프링 부트에 JPA 설정 추가.

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -11,19 +11,20 @@ import hello.hellospring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
-	/*
 	private final DataSource dataSource;
 	public SpringConfig(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
+
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
 	}
+
 	@Bean
 	public MemberRepository memberRepository() {
 		// return new MemoryMemberRepository();
-		return new JdbcMemberRepository(dataSource);
+		// return new JdbcMemberRepository(dataSource);
+		return new JdbcTemplateMemberRepository(dataSource);
 	}
-	*/
 }
